9609c7788b39344969f861d954f53ffd34e6d1f3
denghui <2037276628@qq.com>
2018-12-12 21:07:05 +0800

    Initial Commit

---
+++ XMLYAPP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XMLYAPP
+//
+//  Created by chengdenghui on 2018/12/12.
+//  Copyright © 2018 程登辉. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
